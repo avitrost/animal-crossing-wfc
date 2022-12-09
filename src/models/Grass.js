e74e8b680e95acf6f69823baaffa9b16ba0d3c46
Kiara <michelle_vong@brown.edu>
2022-12-08 21:28:25 -0500

    fix flowers

--- src/models/Grass.js
+++ src/models/Grass.js
@@ -4,8 +4,14 @@ import { useGLTF } from '@react-three/drei'
 export default function useGrass() {
   const { nodes, materials } = useGLTF('assets/grass.glb')
 
-  return useMemo( () => ({
-	geometries: [ nodes.grass.geometry, nodes.grass001.geometry, nodes.grass002.geometry ] ,
+//   return useMemo( () => ({
+// 	geometries: [ nodes.grass.geometry, nodes.grass001.geometry, nodes.grass002.geometry ] ,
+// 	materials: [ materials.grass,materials.grass,materials.grass ],
+//   }), [nodes, materials]);
+// }
+
+return useMemo( () => ({
+	geometries: [ ] ,
 	materials: [ materials.grass,materials.grass,materials.grass ],
   }), [nodes, materials]);
 }
